ef0e21e41e4413471c001e056560ed7f2c575d2a
zaforemon20 <abuzaforemon@gmail.com>
2022-04-23 23:16:43 +0600

    install router and set up routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import NotFound from './components/NotFound/NotFound';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
 
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        {/* <Route path='/review' element={}></Route> */}
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NotFound.css'
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404</h1>
+      <p>Not found the page you looking for</p>
+    </div>
+  );
+};
+
+export default NotFound;
