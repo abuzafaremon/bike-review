01a466e6504ad912ab4a51bdc19df8f878949d3e
zaforemon20 <abuzaforemon@gmail.com>
2022-04-25 19:42:10 +0600

    blogs component complete

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './Blogs.css'
 
 const Blogs = () => {
   return (
-    <div>
-      <h2>Blogs here</h2>
+    <div className='qna-container'>
+      <div className='qna'>
+        <h2>What is Context API?</h2>
+        <p>Ans: The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.
+        </p>
+      </div>
+      <div className='qna'>
+        <h2>What is Semantic Tag?</h2>
+        <p>Ans: Semantic HTML or semantic markup is HTML that introduces meaning to the web page rather than just presentation. For example, a <b>P</b> tag indicates that the enclosed text is a paragraph. This is both semantic and presentational because people know what paragraphs are, and browsers know how to display them.Using semantic tags gives you many more hooks for styling your content, too. Perhaps today you prefer to have your code samples display in the default browser style, but tomorrow, you might want to call them out with a gray background color; later still, you might want to define the precise mono-spaced font family or font stack to use for your samples. You can do all of these things easily by using semantic markup and smartly applied CSS.</p>
+      </div>
+      <div className='qna'>
+        <h2>Inline vs Inline-Block element...</h2>
+        <p>Ans: Compared to display: inline, the major difference is that inline-block allows to set a width and height on the element. Also, with display: inline, top and bottom margins and paddings are not respected, and with display: inline-block they are.
+          Now, the difference between display: inline-block and display: block is that, with display: block, a line break happens after the element, so a block element doesn't sit next to other elements.</p>
+      </div>
     </div>
   );
 };
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,63 @@
 import React from 'react';
+import { BarChart, PieChart, Pie, Bar, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import './Dashboard.css'
 
 const Dashboard = () => {
+  const data = [
+    {
+      "month": "Mar",
+      "investment": 100000,
+      "sell": 241,
+      "revenue": 10401
+    },
+    {
+      "month": "Apr",
+      "investment": 200000,
+      "sell": 423,
+      "revenue": 24500
+    },
+    {
+      "month": "May",
+      "investment": 500000,
+      "sell": 726,
+      "revenue": 67010
+    },
+    {
+      "month": "Jun",
+      "investment": 500000,
+      "sell": 529,
+      "revenue": 40405
+    },
+    {
+      "month": "Jul",
+      "investment": 600000,
+      "sell": 601,
+      "revenue": 50900
+    },
+    {
+      "month": "Aug",
+      "investment": 700000,
+      "sell": 670,
+      "revenue": 61000
+    }
+  ]
   return (
-    <div>
-      <h2>Dashboard</h2>
+    <div className='chart-container'>
+      <BarChart width={600} height={400} data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey='month' />
+        <YAxis />
+        <Tooltip />
+        <Bar dataKey='investment' fill="#8884d8"></Bar>
+        <Bar dataKey='sell' fill="#82ca9d"></Bar>
+        <Bar dataKey='revenue' fill="#12889d"></Bar>
+      </BarChart>
+
+      <PieChart width={730} height={250}>
+        <Tooltip />
+        <Pie data={data} dataKey='revenue' fill='#8884d8' outerRadius={50} cy='50%' cx='50%'></Pie>
+        <Pie data={data} dataKey='revenue' fill='#82ca9d' innerRadius={60} cy='50%' cx='50%' outerRadius={80} label></Pie>
+      </PieChart>
     </div>
   );
 };
